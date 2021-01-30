1a21573990e01a09342340dc3f9428d3dcb01606
Ouwen Huang <ouwen.huang@duke.edu>
2021-01-30 13:14:17 -0500

    Added more autodeploy

--- setup.py
+++ setup.py
@@ -1,11 +1,25 @@
 import setuptools
+from setuptools.command.install import install
+
+VERSION = '0.0.1'
+
+class VerifyVersionCommand(install):
+    """Custom command to verify that the git tag matches our version"""
+    description = 'verify that the git tag matches our version'
+
+    def run(self):
+        tag = os.getenv('CIRCLE_TAG')
+
+        if tag != VERSION:
+            info = "Git tag: {0} does not match the version of this app: {1}".format(tag, VERSION)
+            sys.exit(info)
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="gh-tools-gradient", # Replace with your own username
-    version="0.0.1",
+    version=VERSION,
     author="Ouwen Huang",
     author_email="ouwen.huang@duke.edu",
     description="Gradient Health training tools",
---
+++ tests/utils-test.py
@@ -0,0 +1,6 @@
+import unittest
+from gh_tools import utils
+
+class DummyTest(unittest.TestCase):
+    def test_pass(self):
+        self.assertEqual(1, 1)
