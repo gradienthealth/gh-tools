2ccaeb101eb6daa11eea0fb0554752523ff40131
Ouwen Huang <ouwen.huang@duke.edu>
2021-02-02 19:05:32 -0500

    Added endpoint

--- gh_tools/utils/helpers.py
+++ gh_tools/utils/helpers.py
@@ -3,8 +3,8 @@ import tensorflow as tf
 from datetime import datetime
 import subprocess
 
-def download_and_load_model(path):
+def download_and_load_model(path, endpoint_url='https://s3.wasabisys.com'):
     tmp_dir = '/tmp/' + datetime.now().strftime("%m-%d-%Y-%H:%M:%S")
-    command = "aws s3 sync '{}' '{}' --endpoint-url {} --exact-timestamps".format(path + '/model', tmp_dir)
+    command = "aws s3 sync '{}' '{}' --endpoint-url {} --exact-timestamps".format(path + '/model', tmp_dir, endpoint_url)
     subprocess.run(command, shell=True, env=os.environ.copy())
     return tf.keras.models.load_model(tmp_dir, compile=False)
