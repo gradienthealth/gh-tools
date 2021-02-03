f9e0d913d9f3aee7265398af0ddcb0b8a64610c1
Ouwen Huang <ouwen.huang@duke.edu>
2021-02-02 19:11:07 -0500

    corrected path

--- gh_tools/utils/helpers.py
+++ gh_tools/utils/helpers.py
@@ -5,6 +5,6 @@ import subprocess
 
 def download_and_load_model(path, endpoint_url='https://s3.wasabisys.com'):
     tmp_dir = '/tmp/' + datetime.now().strftime("%m-%d-%Y-%H:%M:%S")
-    command = "aws s3 sync '{}' '{}' --endpoint-url {} --exact-timestamps".format(path + '/model', tmp_dir, endpoint_url)
+    command = "aws s3 sync '{}' '{}' --endpoint-url {} --exact-timestamps".format(path, tmp_dir, endpoint_url)
     subprocess.run(command, shell=True, env=os.environ.copy())
     return tf.keras.models.load_model(tmp_dir, compile=False)
